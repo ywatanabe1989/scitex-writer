6f65099e1c1dc984f6dbfd5c2ad5d1bc4a01e1a3
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2025-05-07 04:58:03 +1000

    Improve PNG to TIFF conversion using DPI scaling.

--- manuscript/scripts/shell/modules/png2tif_all.sh
+++ manuscript/scripts/shell/modules/png2tif_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
-# Timestamp: "2025-05-07 04:39:55 (ywatanabe)"
+# Timestamp: "2025-05-07 04:57:31 (ywatanabe)"
 # File: ./manuscript/scripts/shell/modules/png2tif_all.sh
 
 THIS_DIR="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
@@ -10,20 +10,13 @@ touch "$LOG_PATH" >/dev/null 2>&1
 
 source ./config.src
 
-# png2tif_all(){
-#     for path_png in "$FIGURE_CAPTION_MEDIA_DIR"/Figure_ID_*.png; do
-#         local path_tif="${path_png%.png}.tif"
-#         convert $path_png $path_tif
-#     done
-# }
 png2tif_all(){
-    # find PNGs and convert to TIFF in parallel
     find "$FIGURE_CAPTION_MEDIA_DIR" -maxdepth 1 \
          -name 'Figure_ID_*.png' | \
     parallel --joblog progress.log \
         'in={}; out={.}.tif
-         convert "$in" "$out"
-         echo "TIFF {#}/$(wc -l <<< "$(find '"$FIGURE_CAPTION_MEDIA_DIR"' -maxdepth 1 -name 'Figure_ID_*.png')") -> $out"'
+         convert -density 300 -units PixelsPerInch "$in" "$out"
+         echo "TIFF {#}/'"$(wc -l <<< "$(find '"$FIGURE_CAPTION_MEDIA_DIR"' -maxdepth 1 -name 'Figure_ID_*.png')")"' -> $out"'
 }
 
 png2tif_all 2>&1 | tee -a "$LOG_PATH"
