2bad6526a226d56d0d588564217e2c8de684e259
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2025-05-07 01:18:20 +1000

    Update cleanup.sh to source config.src

--- manuscript/scripts/shell/modules/cleanup.sh
+++ manuscript/scripts/shell/modules/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
-# Timestamp: "2025-05-06 22:14:27 (ywatanabe)"
+# Timestamp: "2025-05-06 23:09:26 (ywatanabe)"
 # File: ./manuscript/scripts/shell/modules/cleanup.sh
 
 THIS_DIR="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
@@ -8,7 +8,7 @@ LOG_PATH="$THIS_DIR/.$(basename $0).log"
 touch "$LOG_PATH" >/dev/null 2>&1
 
 
-source ./scripts/shell/modules/config.src
+source ./config.src
 echo_info "$0..."
 
 
