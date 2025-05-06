25fc19dc364eaf4eba0e2f2994ef7aa619a7e12f
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2025-05-07 01:18:20 +1000

    Update pptx2tif_all.sh to use FIGURE_CAPTION_MEDIA_DIR

--- manuscript/scripts/shell/modules/pptx2tif_all.sh
+++ manuscript/scripts/shell/modules/pptx2tif_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
-# Timestamp: "2025-05-06 20:50:09 (ywatanabe)"
+# Timestamp: "2025-05-06 23:39:01 (ywatanabe)"
 # File: ./manuscript/scripts/shell/modules/pptx2tif_all.sh
 
 THIS_DIR="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
@@ -10,11 +10,11 @@ touch "$LOG_PATH" >/dev/null 2>&1
 
 echo -e "$0 ..."
 
-source ./scripts/shell/modules/config.src
+source ./config.src
 
 # PowerPoint to TIF
-total=$(ls "$FIGURE_SRC_DIR"/Figure_ID_*.pptx | wc -l)
-ls "$FIGURE_SRC_DIR"/Figure_ID_*.pptx | \
+total=$(ls "$FIGURE_CAPTION_MEDIA_DIR"/Figure_ID_*.pptx | wc -l)
+ls "$FIGURE_CAPTION_MEDIA_DIR"/Figure_ID_*.pptx | \
 parallel --eta --progress --joblog progress.log \
     './scripts/shell/modules/pptx2tif_single.sh -i "$(realpath {})" -o "$(realpath {.}.tif)"; \
     echo "Processed: {#}/$total"'
