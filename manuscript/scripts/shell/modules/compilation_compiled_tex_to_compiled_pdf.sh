f6e2d5e5149b7ad7944648f660beecc158436a31
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2025-05-07 01:18:20 +1000

    Update compilation_compiled_tex_to_compiled_pdf.sh to source config.src

--- manuscript/scripts/shell/modules/compilation_compiled_tex_to_compiled_pdf.sh
+++ manuscript/scripts/shell/modules/compilation_compiled_tex_to_compiled_pdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
-# Timestamp: "2025-05-06 22:19:57 (ywatanabe)"
+# Timestamp: "2025-05-06 23:09:27 (ywatanabe)"
 # File: ./manuscript/scripts/shell/modules/compilation_compiled_tex_to_compiled_pdf.sh
 
 THIS_DIR="$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)"
@@ -8,7 +8,7 @@ LOG_PATH="$THIS_DIR/.$(basename $0).log"
 touch "$LOG_PATH" >/dev/null 2>&1
 
 
-source ./scripts/shell/modules/config.src
+source ./config.src
 echo_info "$0 ..."
 
 compile_compiled_tex() {
