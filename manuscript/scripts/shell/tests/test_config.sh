5d68c1dace4dbe577aee32682761318ac06beddb
Yusuke Watanabe <ywatanabe@alumni.u-tokyo.ac.jp>
2025-05-07 01:18:21 +1000

    Update test_config.sh

--- manuscript/scripts/shell/tests/test_config.sh
+++ manuscript/scripts/shell/tests/test_config.sh
@@ -29,24 +29,24 @@ test_function() {
 echo "Testing config.src..."
 
 # Test directory variables are set
-test_function "FIGURE_SRC_DIR is set" "[ -n \"$FIGURE_SRC_DIR\" ]"
+test_function "FIGURE_CAPTION_MEDIA_DIR is set" "[ -n \"$FIGURE_CAPTION_MEDIA_DIR\" ]"
 test_function "FIGURE_JPG_DIR is set" "[ -n \"$FIGURE_JPG_DIR\" ]"
 test_function "FIGURE_COMPILED_DIR is set" "[ -n \"$FIGURE_COMPILED_DIR\" ]"
 test_function "FIGURE_HIDDEN_DIR is set" "[ -n \"$FIGURE_HIDDEN_DIR\" ]"
 
-test_function "TABLE_SRC_DIR is set" "[ -n \"$TABLE_SRC_DIR\" ]"
+test_function "TABLE_CAPTION_MEDIA_DIR is set" "[ -n \"$TABLE_CAPTION_MEDIA_DIR\" ]"
 test_function "TABLE_COMPILED_DIR is set" "[ -n \"$TABLE_COMPILED_DIR\" ]"
 test_function "TABLE_HIDDEN_DIR is set" "[ -n \"$TABLE_HIDDEN_DIR\" ]"
 
 test_function "WORDCOUNT_DIR is set" "[ -n \"$WORDCOUNT_DIR\" ]"
 
 # Check if directories exist or can be created
-test_function "FIGURE_SRC_DIR exists or can be created" "mkdir -p \"$FIGURE_SRC_DIR\" && [ -d \"$FIGURE_SRC_DIR\" ]"
+test_function "FIGURE_CAPTION_MEDIA_DIR exists or can be created" "mkdir -p \"$FIGURE_CAPTION_MEDIA_DIR\" && [ -d \"$FIGURE_CAPTION_MEDIA_DIR\" ]"
 test_function "FIGURE_JPG_DIR exists or can be created" "mkdir -p \"$FIGURE_JPG_DIR\" && [ -d \"$FIGURE_JPG_DIR\" ]"
 test_function "FIGURE_COMPILED_DIR exists or can be created" "mkdir -p \"$FIGURE_COMPILED_DIR\" && [ -d \"$FIGURE_COMPILED_DIR\" ]"
 test_function "FIGURE_HIDDEN_DIR exists or can be created" "mkdir -p \"$FIGURE_HIDDEN_DIR\" && [ -d \"$FIGURE_HIDDEN_DIR\" ]"
 
-test_function "TABLE_SRC_DIR exists or can be created" "mkdir -p \"$TABLE_SRC_DIR\" && [ -d \"$TABLE_SRC_DIR\" ]"
+test_function "TABLE_CAPTION_MEDIA_DIR exists or can be created" "mkdir -p \"$TABLE_CAPTION_MEDIA_DIR\" && [ -d \"$TABLE_CAPTION_MEDIA_DIR\" ]"
 test_function "TABLE_COMPILED_DIR exists or can be created" "mkdir -p \"$TABLE_COMPILED_DIR\" && [ -d \"$TABLE_COMPILED_DIR\" ]"
 test_function "TABLE_HIDDEN_DIR exists or can be created" "mkdir -p \"$TABLE_HIDDEN_DIR\" && [ -d \"$TABLE_HIDDEN_DIR\" ]"
 
